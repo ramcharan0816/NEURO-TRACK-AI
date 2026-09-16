ba4d7d4df0215fc023f95d32663d70f6fa85d8e8
RAMCHARAN <charansriram988@gmail.com>
2026-09-16 14:52:34 +0530

    Update settings.py

--- stress_project/settings.py
+++ stress_project/settings.py
@@ -1,4 +1,3 @@
-```python
 from pathlib import Path
 import os
 
@@ -22,7 +21,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -66,7 +64,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
@@ -75,15 +72,10 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Media files
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Upload folder for analyzed images
 UPLOAD_FOLDER = BASE_DIR / 'media' / 'uploads'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-```
